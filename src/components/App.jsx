97074b37aa55d33b8ca566ee75c7bdc50df2d362
howardhokyin <howard.hyca@gmail.com>
2024-02-16 17:52:01 -0700

    working on layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
 import Card from './Card';
+import '../index.css';
 
 function App() {
   return (
-    <div>
-      <Card />
+    <div className="flex flex-row justify-center min-h-screen bg-red-400 ">
+      <div className="w-1/3 grid grid-cols-2 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="w-2/3 bg-yellow-300">pokemon detail</div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,19 @@ import Charmander from '../../public/charmander.png';
 
 const Card = () => {
   return (
-    <div className="card">
-      <h1>pokemon name</h1>
-      <img src={Charmander}></img>
-      <p>other fields</p>
+    <div className="card ">
+      <div className="flex flex-row border-2 border-black rounded-lg  m-5 max-w-[300px]">
+        <div className="left-container flex items-center justify-center  m-[1px] min-w-[50%]">
+          {' '}
+          <img src={Charmander} alt="" className="" />
+        </div>
+        <div className="right-container flex flex-col justify-center p-2">
+          {' '}
+          <h1>Charmander</h1>
+          <p>Height: 30</p>
+          <p>Weight: 30</p>
+        </div>
+      </div>
     </div>
   );
 };
